6ef5725718de1c9b0b745b66449ef002d08a31f7
Warut Surapat <wsurapat@gilt.jp>
2019-08-14 11:21:25 +0900

    Remove dist folder from tracking.

--- dist/controllers/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getApi = (req, res) => {
-    res.render("api/index", {
-        title: "API Examples"
-    });
-};
-//# sourceMappingURL=api.js.map
